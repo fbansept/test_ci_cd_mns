8a3b0e5bfa1d51edf407305f4fc3c0c3ed22aad6
fbansept <bansept.franck@gmail.com>
2023-12-06 10:01:13 +0100

    comit 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on porteeee ! ${port}`);
+  console.log(`Server listening on porteeeeffffff ! ${port}`);
 });
 
 module.exports = app; // Pour les tests
